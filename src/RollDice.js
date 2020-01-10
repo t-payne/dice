47cc4ef0846cc96a69333bfab099b84dbb25d802
Taylor Payne <taylor_payne@byu.edu>
2020-01-10 09:38:24 -0700

    Create basic structure for RollDice component
    - structure component to that the number of Die components can
      be passed in by prop, but defaults to 2 dice.
    - make roll button show "Rolling..." for 1 second when clicked.

---
+++ src/RollDice.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './RollDice.css';
+
+class RollDice extends React.Component {
+  static defaultProps = { num: 2 }
+
+  constructor(props) {
+      super(props);
+      this.state = { 
+        num: props.num,
+        isRolling: false
+      };
+      this.roll = this.roll.bind(this);
+      this.endRoll = this.endRoll.bind(this);
+  }
+  
+  genDieValue() {
+    return Math.floor(Math.random() * 6) + 6;
+  }
+
+  endRoll(e) {
+    this.setState({ isRolling: false });
+  }
+
+  roll(e) {
+    this.setState({ isRolling: true });
+    setTimeout(this.endRoll, 1000);
+  }
+
+  duplicate = (x, n) => Array.from(new Array(n), () => x);
+
+  render() {
+      return(
+        <div>
+          <div>
+            {this.duplicate("Die", this.props.num)}
+          </div>
+          <div>
+            <button onClick={this.roll}>
+                  { this.state.isRolling ? "Rolling..." : "Roll" }
+            </button>
+          </div>
+        </div>
+      )
+  }
+}
+
+export default RollDice;
